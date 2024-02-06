ebdd216873a33fefc63fe539bb67c880e3511908
Sagar <sagar.arora198@gmail.com>
2024-02-06 21:56:00 +0530

    day 4

--- arithematic.js
+++ arithematic.js
@@ -9,16 +9,17 @@
  * @throws {Error} Value is not integer
  * @throws {Error} value is not a number 
  */
-function checkArrayElement(arrayInput,length){
-    for(let i=0;i<length;i++){
-        if(typeof arrayInput[i]!= "number" || arrayInput[i]>9){
+function checkArrayElement(arrayInput, length) {
+    // loop through array and check if each element is number and less then 9
+    for (let i = 0; i < length; i++) {
+        if (typeof arrayInput[i] != "number" || arrayInput[i] > 9) {
             throw new Error("Value should be a number and less then 9")
         }
-        else if(arrayInput[i]%1!=0){
+        else if (arrayInput[i] % 1 != 0) {
             throw new Error("Only integer should be passed")
         }
     }
-    
+
 }
 
 /** takeInput will take input of 2 arrays and will check if input is correct or not
@@ -26,68 +27,70 @@ function checkArrayElement(arrayInput,length){
  * @param {[]} secondNumberArray represent the second array
  * @throws {Error} no value passed (undefined)
  */
-function takeInput(firstNumberArray,secondNumberArray,maxArray,minArray){
-    if(firstNumberArray==undefined || secondNumberArray==undefined){
+function takeInput(firstNumberArray, secondNumberArray, maxArray, minArray) {
+    // throw error when number input is not a number
+    if (firstNumberArray == undefined || secondNumberArray == undefined) {
         throw new Error("Array not passed")
 
     }
-    else{
+    else {
         // for checking the first number array
-        checkArrayElement(firstNumberArray,firstNumberArray.length)
+        checkArrayElement(firstNumberArray, firstNumberArray.length)
         // for checking the second number array
-        checkArrayElement(secondNumberArray,secondNumberArray.length)
+        checkArrayElement(secondNumberArray, secondNumberArray.length)
     }
-    if(maxArray && minArray ){
-        if(firstNumberArray.length>secondNumberArray.length){
-            maxArray.length=0
-            minArray.length=0
+    // optional argument will return sign to indicate maximum between two array
+    if (maxArray && minArray) {
+        if (firstNumberArray.length > secondNumberArray.length) {
+            maxArray.length = 0
+            minArray.length = 0
             maxArray.push(...firstNumberArray)
             minArray.push(...secondNumberArray)
             return 1
 
 
         }
-        else if(firstNumberArray.length == secondNumberArray.length){
+        else if (firstNumberArray.length == secondNumberArray.length) {
             let lengthOfArray = 0
             let numberFromArrayOne = 0
             let numberFromArrayTwo = 0
-            while(lengthOfArray!=(firstNumberArray.length)){
-                numberFromArrayOne = numberFromArrayOne*10 + firstNumberArray[lengthOfArray]
-                numberFromArrayTwo = numberFromArrayTwo*10 + secondNumberArray[lengthOfArray]
+            while (lengthOfArray != (firstNumberArray.length)) {
+                numberFromArrayOne = numberFromArrayOne * 10 + firstNumberArray[lengthOfArray]
+                numberFromArrayTwo = numberFromArrayTwo * 10 + secondNumberArray[lengthOfArray]
                 lengthOfArray++
 
             }
-            if(numberFromArrayOne>numberFromArrayTwo){
-                maxArray.length =0
-                minArray.length =0
+            if (numberFromArrayOne > numberFromArrayTwo) {
+                maxArray.length = 0
+                minArray.length = 0
                 maxArray.push(...firstNumberArray)
                 minArray.push(...secondNumberArray)
                 return 1
             }
-            else{
-                maxArray.length =0
-                minArray.length =0
+            else {
+                maxArray.length = 0
+                minArray.length = 0
                 minArray.push(...firstNumberArray)
                 maxArray.push(...secondNumberArray)
                 return -1
-                
+
 
             }
 
         }
-        else{
+        else {
 
-            maxArray.length=0
-            minArray.length=0
+            maxArray.length = 0
+            minArray.length = 0
             maxArray.push(...secondNumberArray)
             minArray.push(...firstNumberArray)
             return -1
-            
+
         }
     }
 
 
-    
+
 }
 
 /** add the array and return the resultant array
@@ -99,56 +102,58 @@ function takeInput(firstNumberArray,secondNumberArray,maxArray,minArray){
  * @throws {Error} value is not a number 
  * @throws {Error} no value passed (undefined)
  */
-function addArray(firstArray,secondArray){
-    takeInput(firstArray,secondArray)
+function addArray(firstArray, secondArray) {
+    takeInput(firstArray, secondArray)
+
     // make a while loop that will add the last element of array and update the position of arrays index using 2 pointers
-    let firstPointer = firstArray.length-1
-    let secondPointer = secondArray.length-1
-    
+    let firstPointer = firstArray.length - 1
+    let secondPointer = secondArray.length - 1
+
     let resultant = []
-    let carry =0
+    let carry = 0
 
-    
-    while(firstPointer>=0 && secondPointer >=0){
-        let sum =firstArray[firstPointer]+secondArray[secondPointer] + carry
+
+    while (firstPointer >= 0 && secondPointer >= 0) {
+        let sum = firstArray[firstPointer] + secondArray[secondPointer] + carry
         // console.log(sum);
-        
-        if(sum>9){
-            carry = Math.floor(sum/10)
-            resultant.unshift(sum%10)
+
+        if (sum > 9) {
+            carry = Math.floor(sum / 10)
+            resultant.unshift(sum % 10)
 
         }
-        else{
-            
+        else {
+
             resultant.unshift(sum)
-            carry =0
+            carry = 0
 
-            
+
         }
-        firstPointer = firstPointer -1
+        firstPointer = firstPointer - 1
         secondPointer = secondPointer - 1
-        
+
 
     }
     // find the remaining array from the two array by comparing the pointer length
-    
-   
-    let remainingArray = firstPointer>=0? firstArray:secondArray
-    let remainingLength = firstPointer>=0 ? firstPointer : secondPointer
-    
-    
+
+
+    let remainingArray = firstPointer >= 0 ? firstArray : secondArray
+    let remainingLength = firstPointer >= 0 ? firstPointer : secondPointer
+
+
     // pushing the remaining array to the resultant
-    for(let i=remainingLength;i>=0;i--){
-        if(carry+remainingArray[i]>9){
-            carry = Math.floor((carry+remainingArray[i])/10)
-            resultant.unshift((carry+remainingArray[i])%10)
+    for (let i = remainingLength; i >= 0; i--) {
+        if (carry + remainingArray[i] > 9) {
+            carry = Math.floor((carry + remainingArray[i]) / 10)
+            resultant.unshift((carry + remainingArray[i]) % 10)
         }
-        else{
-            resultant.unshift(carry+remainingArray[i])
-            carry =0
+        else {
+            resultant.unshift(carry + remainingArray[i])
+            carry = 0
         }
     }
-    if(carry>0){
+    // if carry is still remaining 
+    if (carry > 0) {
         resultant.unshift(carry)
     }
 
@@ -170,74 +175,63 @@ function addArray(firstArray,secondArray){
  * @throws {Error} value is not a number 
  * @throws {Error} no value passed (undefined)
  */
-function subtractArray(firstArray,secondArray){
-    let maxArray =[]
+function subtractArray(firstArray, secondArray) {
+    // maxArray will store the maximum number of two array
+    // minArray will store the minimum number of two array
+    
+    let maxArray = []
     let minArray = []
-    let resultant = []
-    let carry =0
-    let condition = 0
-    if(firstArray[0]=='-' ){
-        firstArray.shift()
-        condition = -1
 
-    }
-    else if(secondArray[0]=='-'){
-        secondArray.shift()
-        condition = 1
-    }
+    //resultant for storing the the result of operation
+    let resultant = []
 
     //check for input and also which array is maximum or minimum
-    let sign = takeInput(firstArray,secondArray,maxArray,minArray)
-    if(condition==0){
+    // sign represent if we are subtracting a larger number from smaller number
+    let sign = takeInput(firstArray, secondArray, maxArray, minArray)
+    
+    
+    // to store the carry value after subtraction 
+    let carry = 0
+    //using 2 pointer approch
+    let maxArrayPointer = maxArray.length - 1
+    let minArrayPointer = minArray.length - 1
+    // taking a carry which will add to the next number 
+    // if number is smaller then it will add 10 to it so subtraction
+    while (minArrayPointer >= 0) {
+        if (minArray[minArrayPointer] > (maxArray[maxArrayPointer] + carry)) {
+            maxArray[maxArrayPointer] = 10 + (maxArray[maxArrayPointer] + carry)
+            carry = -1
+            resultant.unshift(maxArray[maxArrayPointer] - minArray[minArrayPointer])
 
-        
-            //using 2 pointer approch
-            let maxArrayPointer = maxArray.length -1
-            let minArrayPointer = minArray.length -1
-            // taking a carry which will add to the next number 
-        // if number is smaller then it will add 10 to it so subtraction
-        while(minArrayPointer>=0){
-            if(minArray[minArrayPointer]>(maxArray[maxArrayPointer]+carry)){
-                maxArray[maxArrayPointer] = 10 + (maxArray[maxArrayPointer]+carry)
-                carry = -1
-                resultant.unshift(maxArray[maxArrayPointer]-minArray[minArrayPointer])
-                
-                
-                
-            }
-            else{
-                resultant.unshift(((maxArray[maxArrayPointer]+carry)-minArray[minArrayPointer]))
-                carry = 0
-            }
-            minArrayPointer--
-            maxArrayPointer--
+
+
         }
-        // for remaining element
-        for(let i=maxArrayPointer;i>=0;i--){
-            if(carry+maxArray[i]>=0){
-                resultant.unshift(carry+maxArray[i])
-                carry = 0
-                
-            }
-            else{
-                resultant.unshift(9)
-            }
+        else {
+            resultant.unshift(((maxArray[maxArrayPointer] + carry) - minArray[minArrayPointer]))
+            carry = 0
         }
-        // for removing leading zeros
-        
-        while(resultant[0]==0){
-            resultant.shift()
+        minArrayPointer--
+        maxArrayPointer--
+    }
+    // for remaining element
+    for (let i = maxArrayPointer; i >= 0; i--) {
+        if (carry + maxArray[i] >= 0) {
+            resultant.unshift(carry + maxArray[i])
+            carry = 0
+
         }
-        if(sign == -1){
-            resultant.unshift("-")
+        else {
+            resultant.unshift(9)
         }
     }
-    else if(condition==-1){
-        resultant = addArray(firstArray,secondArray)
+    // for removing leading zeros
+
+    while (resultant[0] == 0) {
+        resultant.shift()
+    }
+    // if we are subtracting larger number from smaller then add a -ve sign 
+    if (sign == -1) {
         resultant.unshift("-")
-    }
-    else{
-        resultant = addArray(firstArray,secondArray)
     }
 
     return resultant
@@ -257,39 +251,28 @@ function subtractArray(firstArray,secondArray){
  * @throws {Error} value is not a number 
  * @throws {Error} no value passed (undefined)
  */
-function multiplication(firstArray,secondArray){
+function multiplication(firstArray, secondArray) {
+    // create a resultant array which will store the result of operations
     let resultant = []
-    let condition = 0
-    if(firstArray[0]=='-' && secondArray[0]=='-'){
-        condition = 0
-        firstArray.shift()
-        secondArray.shift()
-    }
-    else if(firstArray[0]=='-'){
-        condition = -1
-        firstArray.shift()
-    }
-    else if(secondArray[0]=='-'){
-        condition = -1
-        secondArray.shift()
-    }
-
-    
-    takeInput(firstArray,secondArray)
+    // call takeInput to validate the arrays
+    takeInput(firstArray, secondArray)
+    //find the integer number from second array
     let lengthOfArray = 0
     let numberFromArray = 0
-    while(lengthOfArray!=(secondArray.length)){
-        numberFromArray = numberFromArray*10 + secondArray[lengthOfArray]
+    if(secondArray.length>7){
+        throw new Error("Number too large cannot perform such long operation."+
+        "Optimised function in progress")
+    }
+    while (lengthOfArray != (secondArray.length)) {
+        numberFromArray = numberFromArray * 10 + secondArray[lengthOfArray]
         lengthOfArray++
 
     }
-    
-    for(let i=0;i<numberFromArray;i++){
+    // multiply by continuously calling the addArray function and adding
 
-        resultant = addArray(resultant,firstArray)
-    }
-    if(condition==-1){
-        resultant.unshift('-')
+    for (let i = 0; i < numberFromArray; i++) {
+
+        resultant = addArray(resultant, firstArray)
     }
 
 
@@ -298,18 +281,140 @@ function multiplication(firstArray,secondArray){
 
 
 
+/** Utility function, let you perform all arithmetic operation and log the output
+ * 
+ * @param {[]} firstArray First array to input 
+ * @param {*} secondArray Second array to input
+ * @param {String} arithmeticOperation  Operation you want to perform
+ * 
+ */
+function doArithmeticOperation(firstArray, secondArray, arithmeticOperation) {
+    // create a resultant array which will store the resultant
+    // of the arithmetic operations
+    let resultantArray = []
+
+    // check for the given input of arithmetic operator and perform operation
+    switch (arithmeticOperation) {
+        // handel the case for negative number array and call function accordingly
+        // remove the '-' sign from the array and perform specified operation 
+        // add sign at last accordingly
+        case '-':
+            if (firstArray[0] == '-' && secondArray[0] == '-') {
+                firstArray.shift()
+                secondArray.shift()
+                resultantArray = subtractArray(secondArray, firstArray)
+                console.log(resultantArray);
+            }
+            else if (firstArray[0] == '-') {
+                firstArray.shift()
+                resultantArray = addArray(firstArray, secondArray)
+                resultantArray.unshift('-')
+                console.log(resultantArray);
+
+
+            }
+            else if (secondArray[0] == '-') {
+                secondArray.shift()
+                resultantArray = addArray(firstArray, secondArray)
+                console.log(resultantArray);
+            }
+            else {
+                resultantArray = subtractArray(firstArray, secondArray)
+                console.log(resultantArray);
+
+            }
+
+            break;
+        case '+':
+            if (firstArray[0] == '-' && secondArray[0] == '-') {
+                firstArray.shift()
+                secondArray.shift()
+                resultantArray = addArray(firstArray, secondArray)
+                resultantArray.unshift('-')
+                console.log(resultantArray);
+            }
+            else if (firstArray[0] == '-') {
+                firstArray.shift()
+                resultantArray = subtractArray(secondArray, firstArray)
+                console.log(resultantArray);
+
+            }
+            else if (secondArray[0] == '-') {
+                secondArray.shift()
+                resultantArray = subtractArray(firstArray, secondArray)
+                console.log(resultantArray);
+            }
+            else {
+                resultantArray = addArray(firstArray, secondArray)
+                console.log(resultantArray);
+            }
+            break
+        case '*':
+            if (firstArray[0] == '-' && secondArray[0] == '-') {
+                firstArray.shift()
+                secondArray.shift()
+                resultantArray = multiplication(firstArray, secondArray)
+                console.log(resultantArray);
+            }
+            else if (firstArray[0] == '-') {
+                firstArray.shift()
+                resultantArray = multiplication(firstArray, secondArray)
+                resultantArray.unshift('-')
+                console.log(resultantArray);
+            }
+            else if (secondArray[0] == '-') {
+                secondArray.shift()
+                resultantArray = multiplication(firstArray, secondArray)
+                resultantArray.unshift('-')
+                console.log(resultantArray);
+            }
+            else {
+                resultantArray = multiplication(firstArray, secondArray)
+                console.log(resultantArray);
+            }
+            break
+        case '/':
+            console.log("Work in Progress");
+            break
+        // if any other operator is getting input then throw error    
+        default:
+            throw new Error("Arithmatic operation not correct " +
+                "give appropriate input('+' , '-' , '*', '/')")
+            break;
+    }
+}
+
 /**
  * 
  * @param {[]} firstArray input array 1 
  * @param {[]} secondArray input array 2
  * @returns {[]} resultant array after division
  */
-// function division(firstArray,secondArray){
+function division(firstArray,secondArray){
 
-//     return []
-// }
-console.log(addArray([1,9,2,4],[4,5,6]));
+    return []
+}
 
-console.log(subtractArray([1,0,0,0],['-',9,9,9,9,1]));
+try {
+    
+    doArithmeticOperation([1,9,2,4],[4,5,6],'+')
+    
+    doArithmeticOperation([1,2,3,4],[1,2,3],'-')
+    
+    doArithmeticOperation([1,0,0,0],[9,9,9,9,1],'-')
+    
+    doArithmeticOperation(['-',1,2,3,4],['-',1,2,3],'-')
+    
+    doArithmeticOperation([1,2,3,4],[1,2,3],'*')
+    
+    doArithmeticOperation([1,2,3,4],[1,2,3],'+')
+} catch (error) {
+    console.log(error);
+    
+}
 
-console.log(multiplication(['-',1,2,3],['-',2,1,2]));
+
+
+function newMultiplication(firstArray,secondArray){
+
+}
